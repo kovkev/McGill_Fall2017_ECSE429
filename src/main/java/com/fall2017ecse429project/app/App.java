1c11d67995dd7c7939343ea74d719de02af02935
Kevin Galkov <kevin@galkov.com>
2017-10-03 14:59:14 -0400

    Add pom.xml , src/ and src/test

---
+++ src/main/java/com/fall2017ecse429project/app/App.java
@@ -0,0 +1,29 @@
+package com.fall2017ecse429project.app;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Integer[] arr = new Integer[]{1, 2};
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        l.add(5);
+        l.add(6);
+
+        CollectionUtils.addAll(l, arr);
+
+        System.out.println( "\""
+                + Integer.toString(l.get(2))
+                + "\""
+                + " should be 5."
+        );
+    }
+}
