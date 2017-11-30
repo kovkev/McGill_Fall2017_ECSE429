2c592a6caab4b3dd22450b7b312b23bb9bb00eaa
ilyasamaniss <ilyasamaniss@mail.mcgill.ca>
2017-11-30 15:46:41 -0500

    It is just a test. Some methods were added to the app.java class

--- src/main/java/com/fall2017ecse429project/app/App.java
+++ src/main/java/com/fall2017ecse429project/app/App.java
@@ -2,8 +2,12 @@ package com.fall2017ecse429project.app;
 
 import org.apache.commons.collections4.*;
 import org.apache.commons.collections4.bag.CollectionBag;
+import org.apache.commons.collections4.bag.CollectionSortedBag;
 import org.apache.commons.collections4.bag.HashBag;
+import org.apache.commons.collections4.bag.PredicatedSortedBag;
 import org.apache.commons.collections4.bag.TransformedBag;
+import org.apache.commons.collections4.bag.TreeBag;
+import org.apache.commons.collections4.functors.NotNullPredicate;
 import org.apache.commons.collections4.iterators.UnmodifiableMapIterator;
 import org.apache.commons.collections4.map.HashedMap;
 
@@ -38,7 +42,61 @@ public class App
 
         test_map_iterator();
         test_transformer();
+        test_Bag_Group();
     }
+    
+    public static void test_Bag_Group() {
+    	test_Bag();
+    	test_SortedBag();
+    	test_BagUtils();
+    }
+    
+    // Since a Bag instance cannot be instantiated, the HashBag was used
+    // to test the Bag class
+    public static void test_Bag() {
+    	Bag bag = new HashBag();
+    	
+    	// Add an element to the bag
+    	bag.add("apple", 3);
+    	System.out.println("*");
+    	assert bag.getCount("apple") == 3;
+    	
+    	// Remove only one unit of the element 
+    	// Two elements should be remaining
+    	bag.remove("apple", 1);
+    	assert bag.getCount("apple") == 2;
+    	
+    	// Remove all elements
+    	bag.remove("apple");
+    	assert bag.getCount("apple") == 0; 
+    }
+    
+    // In order to test the SortedBag class and its inheritance from the class Bag
+    // The inherited methods were tested in the following tests
+    public static void test_SortedBag() {
+    	
+    	Bag bag = BagUtils.synchronizedBag(new HashBag());
+    	// This will unsure that no null entries are added to the bag
+    	SortedBag sortedBag = PredicatedSortedBag.predicatedSortedBag(new TreeBag(), NotNullPredicate.INSTANCE);
+    	 
+    	// Add an element to the bag
+    	sortedBag.add("banana", 10);
+    	assert sortedBag.getCount("banana") == 10;
+
+    	// Remove only one unit of the element 
+    	// Two elements should be remaining
+    	sortedBag.remove("banana", 1);
+    	assert sortedBag.getCount("banana") == 9;
+
+    	// Remove all elements
+    	sortedBag.remove("banana");
+    	assert sortedBag.getCount("apple") == 0; 
+    }
+    
+    public static void test_BagUtils() {
+    	
+    }
+
 
     public static void test_transformer() {
         test_constant_transformingBag();
