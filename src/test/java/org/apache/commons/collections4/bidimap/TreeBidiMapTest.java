23262b73efd07ccf7830c1e6c165d6fdecd6a9d8
Kevin Galkov <kevin@galkov.com>
2017-11-30 21:45:50 -0500

    Add tests for TreeBidiMap

--- src/test/java/org/apache/commons/collections4/bidimap/TreeBidiMapTest.java
+++ src/test/java/org/apache/commons/collections4/bidimap/TreeBidiMapTest.java
@@ -1,6 +1,19 @@
 package org.apache.commons.collections4.bidimap;
 
 import static org.junit.Assert.*;
+
+import java.security.KeyStore;
+import java.security.KeyStore.Entry;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.zip.ZipEntry;
+
+import javax.naming.OperationNotSupportedException;
+import javax.swing.JApplet;
+
+import org.apache.commons.collections4.MapIterator;
+import org.apache.commons.collections4.OrderedBidiMap;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,6 +32,8 @@ public class TreeBidiMapTest {
 
 	@Before
 	public void setUp() throws Exception {
+
+
 	}
 
 	@After
@@ -26,8 +41,149 @@ public class TreeBidiMapTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testMapOrder() {
+		// This test demonstrates that 
+		// Keys where the comparator returns 0 will be overwritten. They can be overwritten even when the new value is smaller.
+		// When a Key is set where the Value compares to one other value in the TreeBidiMap with 0, the key is overwritten;
+		// The map iterators do iterate in a sorted manner.
+		
+		class Node implements Comparable<Node> {
+			public int value;
+			public Node(int value){
+				this.value = value;
+			}
 
+			@Override
+			public int compareTo(Node o) {
+				return value - o.value;
+			}
+		}
+		
+		Node a = new Node(1);
+		Node c1 = new Node(3);
+		Node c2 = new Node(3);
+		Node y = new Node(25);
+		Node z = new Node(26);
+		
+		TreeBidiMap<Node, Integer> tbm = new TreeBidiMap<Node, Integer>();
+		
+		tbm.put(y, 0);
+		tbm.put(a, 2);
+		tbm.put(c2, 4);
+		tbm.put(c1, 3);
+		tbm.put(z, 0);
+		
+		MapIterator<Node, Integer> mapiter = tbm.mapIterator();
+		Integer count = 0;
+		
+		while(mapiter.hasNext()){
+			Node c = mapiter.next();
+			Integer i = mapiter.getValue();
+			
+			// mapiter.setValue(i + 10);
+			
+			System.out.println(c.value);
+			System.out.println(i);
+			
+			if(count == 0){
+				assert c.value == 1;
+				assert i == 2;
+			} else if(count == 1){
+				assert c.value == 3;
+				assert i == 3;
+			} else if ( count == 2){
+				assert c.value == 26;
+				assert i == 0;
+			}
+			
+			count++;
+		}
+		
+		System.out.println("next");
+		
+		OrderedBidiMap<Integer, Node> ibm = tbm.inverseBidiMap();
+		MapIterator<Integer, Node> mi = ibm.mapIterator();
+		
+		count = 0;
+		while (mi.hasNext()) {
+			Integer i = mi.next();
+			Node c = mi.getValue();
+			
+			System.out.println(c.value);
+			System.out.println(i);
+			
+			if (count == 0) {
+				assert c.value == 26;
+				assert i == 0;
+			} else if (count == 1){
+				assert c.value == 1;
+				assert i == 2;
+			} else if (count == 2){
+				assert c.value == 3;
+				assert i == 3;
+			}
+			
+			count++;
+		}
+		
+		System.out.println("AAA");
+	}
+	
+	@Test
+	public void testSetValue() {
+		
+		TreeBidiMap<Character, Integer> tbm =
+			new TreeBidiMap<Character, Integer>();
+		
+		tbm.put('c', 0);
+		
+		Set<java.util.Map.Entry<Character, Integer>> es =
+			tbm.entrySet();
+		
+		java.util.Map.Entry<Character, Integer> e = 
+			es.iterator().next();
+		
+		try {
+			e.setValue(5);
+			assert false;
+		} catch(UnsupportedOperationException exception) {
+			// all good
+		}
+	}
+	
+	@Test
+	public void testSetUpdate() {
+		
+		TreeBidiMap<Character, Integer> tbm =
+			new TreeBidiMap<Character, Integer>();
+		
+		tbm.put('c', 0);
+		
+		Set<java.util.Map.Entry<Character, Integer>> es =
+			tbm.entrySet();
+		
+		Iterator<java.util.Map.Entry<Character, Integer>> iter = es.iterator();
+		
+		java.util.Map.Entry<Character, Integer> e = 
+			iter.next();
+		
+		assert e.getKey() == 'c';
+		assert e.getValue() == 0;
+		
+		assert !iter.hasNext();
+		
+		tbm.put('z', 2);
+		
+		es = tbm.entrySet();
+		assert es.size() == 2;
+		
+		assert !iter.hasNext();
+		
+		OrderedBidiMap<Integer, Character> ibm = tbm.inverseBidiMap();
+		Set<java.util.Map.Entry<Integer, Character>> ies =
+				ibm.entrySet();
+		
+		assert ies.size() == 2;
+	}
 }
+
